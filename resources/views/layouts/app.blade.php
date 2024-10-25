46d96eacc8a6b0d9cefacead09879a7758bdf3fd
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-25 16:28:44 +0200

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        @yield("additional-meta")
+
+        <title>@yield("page-title", "laravel-project")</title>
+
+        @yield("additional-cdn")
+    </head>
+
+    <body>
+        {{-- header --}}
+        @include('partials.header')
+
+        {{-- main content --}}
+        <main>
+            @yield('content')
+        </main>
+
+        {{-- footer --}}
+        @include('partials.footer')
+        @vite('resources/js/app.js')
+    </body>
+</html>
