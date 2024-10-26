5dba564a1438e24bc9189746ab6d781f1692b36e
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-27 00:09:33 +0200

    added in TrainController $date_day with Carbon and fixed query on $trains

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Train;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class TrainController extends Controller
 {
     public function index()
     {
-        $trains = Train::all();
+        $date_day = Carbon::now()->format('Y-m-d');
+        $trains = $trains = Train::where('departure_date', "$date_day")->get();
+
+
         dump($trains);
-        return view("trains.index", compact("trains"));
+        return view("trains.index", compact("trains", "date_day",));
     }
 }
