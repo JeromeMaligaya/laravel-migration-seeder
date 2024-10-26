d631d9cb46d28a6a878d3ef18f307378c0514358
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-27 00:17:08 +0200

    removed dump in TrainController

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -11,10 +11,8 @@ class TrainController extends Controller
     public function index()
     {
         $date_day = Carbon::now()->format('Y-m-d');
-        $trains = $trains = Train::where('departure_date', "$date_day")->get();
+        $trains = $trains = Train::where('departure_date', $date_day)->get();
 
-
-        dump($trains);
         return view("trains.index", compact("trains", "date_day",));
     }
 }
