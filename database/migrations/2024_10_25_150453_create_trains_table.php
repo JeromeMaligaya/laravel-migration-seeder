39e83b2a83731eb00f7a8abcb5bc02c6038301cb
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-26 22:35:46 +0200

    filled trains table

--- database/migrations/2024_10_25_150453_create_trains_table.php
+++ database/migrations/2024_10_25_150453_create_trains_table.php
@@ -12,7 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('trains', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('company', 150);
+            $table->string('departure_station', 150);
+            $table->string('arrival_station', 150);
+            $table->date('departure_date');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code', 10);
+            $table->unsignedTinyInteger('number_of_carriages');
+            $table->boolean('on_time')->default(true);
+            $table->boolean('cancelled')->default(false);
             $table->timestamps();
         });
     }
