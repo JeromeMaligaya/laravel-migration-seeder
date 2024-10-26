b863d18c8e9880a0eddc75cf43d5a5a0a9943c39
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-26 23:58:50 +0200

    added table #trains-table in index.blade.php

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -2,6 +2,39 @@
 
 @section('content')
 <div class="container">
-    <h1>content</h1>
+    <h1>Train List</h1>
+    <p>{{ $date_day }}</p>
+    <table id="trains-table">
+        <thead>
+            <tr>
+                <th>Company</th>
+                <th>Departure Station</th>
+                <th>Arrival Station</th>
+                <th>Departure Date</th>
+                <th>Departure Time</th>
+                <th>Arrival Time</th>
+                <th>Train Code</th>
+                <th>Number of Carriages</th>
+                <th>On Time</th>
+                <th>Cancelled</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($trains as $train)
+                <tr class="single-train">
+                    <td>{{ $train->company }}</td>
+                    <td>{{ $train->departure_station }}</td>
+                    <td>{{ $train->arrival_station }}</td>
+                    <td>{{ $train->departure_date }}</td>
+                    <td>{{ $train->departure_time }}</td>
+                    <td>{{ $train->arrival_time }}</td>
+                    <td>{{ $train->train_code }}</td>
+                    <td>{{ $train->number_of_carriages }}</td>
+                    <td>{{ $train->on_time ? 'Yes' : 'No' }}</td>
+                    <td>{{ $train->cancelled ? 'Yes' : 'No' }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
 @endsection
