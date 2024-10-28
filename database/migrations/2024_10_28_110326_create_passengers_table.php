1bc71f529cacd033056b5436817a5d1ceb2459a8
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-28 12:25:13 +0100

    added column passenger_number

--- database/migrations/2024_10_28_110326_create_passengers_table.php
+++ database/migrations/2024_10_28_110326_create_passengers_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('passengers', function (Blueprint $table) {
             $table->id();
+            $table->tinyInteger("passenger_number")->unsigned()->nullable(false);
             $table->timestamps();
         });
     }
